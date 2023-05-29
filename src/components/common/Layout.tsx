1bccd430107111952afd0f2fbe35d62fc8ac3d88
Yongveloper <aaa9015@naver.com>
2023-05-29 15:56:02 +0900

    [개선] 구글애널리틱스 4

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -31,8 +31,10 @@ const Layout = ({ children }: ILayoutProps) => {
         gtag.pageview(url);
       };
       router.events.on('routeChangeComplete', handleRouteChange);
+      router.events.on('hashChangeComplete', handleRouteChange);
       return () => {
         router.events.off('routeChangeComplete', handleRouteChange);
+        router.events.off('hashChangeComplete', handleRouteChange);
       };
     }
   }, [router.events]);
