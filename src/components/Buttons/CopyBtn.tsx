6d5f6ddffaa1bca0de3550257f97466441e8adf9
Yongveloper <aaa9015@naver.com>
2023-05-29 14:25:56 +0900

    [개선] 불필요한 컴포넌트 제거 및 리팩토링 clipboard API를 사용하는 방식으로 변경하면서 Input은 필요 없어짐, currentURL부분 IIFE방식으로수정

--- src/components/Buttons/CopyBtn.tsx
+++ src/components/Buttons/CopyBtn.tsx
@@ -1,19 +1,12 @@
-import styled from 'styled-components';
 import Button from '../common/Button';
 
-const Clipboard = styled.input.attrs({ type: 'text' })`
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: 0;
-`;
-
 const CopyBtn = () => {
-  let currentUrl = '';
-
-  if (typeof window !== 'undefined') {
-    currentUrl = window.location.href;
-  }
+  const currentURL = (() => {
+    if (typeof window !== 'undefined') {
+      return window.location.href;
+    }
+    return '';
+  })();
 
   const copyHandler = async () => {
     if (!navigator.clipboard) {
@@ -21,19 +14,14 @@ const CopyBtn = () => {
     }
 
     try {
-      await navigator.clipboard.writeText(currentUrl);
+      await navigator.clipboard.writeText(currentURL);
       alert('현재 주소가 클립보드에 복사되었습니다.');
     } catch (err) {
       alert('복사하는데 실패했습니다:' + err);
     }
   };
 
-  return (
-    <>
-      <Button onClick={copyHandler}>링크 복사하기</Button>
-      <Clipboard value={currentUrl} readOnly />
-    </>
-  );
+  return <Button onClick={copyHandler}>링크 복사하기</Button>;
 };
 
 export default CopyBtn;
