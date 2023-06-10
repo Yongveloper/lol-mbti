1b7dade2f44312b9fcbb66204345dc0501aeb9ac
Yongveloper <aaa9015@naver.com>
2023-06-10 15:20:46 +0900

    [변경] 상수-> enum, 함수 분리 리팩토링

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,26 +1,33 @@
 import { useCallback, useEffect, useContext } from 'react';
 import { Theme, ThemeContext } from 'src/context/ThemeProvider';
 
+enum ThemeMode {
+  LIGHT = 'light',
+  DARK = 'dark',
+}
+const LS_KEY = 'theme';
+
+const getThemeFromLocalStorage = (): ThemeMode => {
+  const theme = window.localStorage.getItem(LS_KEY);
+  return theme === ThemeMode.DARK ? ThemeMode.DARK : ThemeMode.LIGHT;
+};
+
 const useDarkMode = (): [string | undefined, () => void] => {
-  const LS_KEY = 'theme';
-  const LIGHT = 'light';
-  const DARK = 'dark';
   const { themeMode, setThemeMode } = useContext(ThemeContext) as Theme;
 
   const switchTheme = useCallback(() => {
-    if (themeMode === LIGHT) {
-      window.localStorage.setItem(LS_KEY, DARK);
-      setThemeMode(DARK);
+    if (themeMode === ThemeMode.LIGHT) {
+      window.localStorage.setItem(LS_KEY, ThemeMode.DARK);
+      setThemeMode(ThemeMode.DARK);
     } else {
-      window.localStorage.setItem(LS_KEY, LIGHT);
-      setThemeMode(LIGHT);
+      window.localStorage.setItem(LS_KEY, ThemeMode.LIGHT);
+      setThemeMode(ThemeMode.LIGHT);
     }
   }, [themeMode, setThemeMode]);
 
   useEffect(() => {
-    const localTheme = window.localStorage.getItem(LS_KEY);
-    const initialState = localTheme ? localTheme : LIGHT;
-    setThemeMode(initialState);
+    const localTheme = getThemeFromLocalStorage();
+    setThemeMode(localTheme);
   }, [setThemeMode]);
 
   return [themeMode, switchTheme];
