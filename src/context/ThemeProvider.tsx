d1ad9986e80b8eb191aa7549e90978ab60212227
Yongveloper <aaa9015@naver.com>
2023-10-30 23:46:45 +0900

    refactor: 불필요한 enum과 useEffect 제거 enum 대신 상수로 관리하는 것이 더 깔끔하다고 판단. 다크모드 여부 확인 로직(useEffect) 중복 제거

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -2,6 +2,9 @@ import { lightTheme, darkTheme } from '../../styles/theme';
 import { createContext, useEffect, useState } from 'react';
 import { ThemeProvider as StyledProvider } from 'styled-components';
 
+export const LIGHT = 'light';
+export const DARK = 'dark';
+export const LS_KEY = 'theme';
 export interface Theme {
   themeMode: string;
   setThemeMode: React.Dispatch<React.SetStateAction<string>>;
@@ -9,12 +12,15 @@ export interface Theme {
 
 export const ThemeContext = createContext<Theme | null>(null);
 
+const getThemeByLocalStorage = () => localStorage.getItem(LS_KEY) || LIGHT;
+
 const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [themeMode, setThemeMode] = useState('light');
-  const themeObject = themeMode === 'light' ? lightTheme : darkTheme;
+  const [themeMode, setThemeMode] = useState(LIGHT);
+  const themeObject = themeMode === LIGHT ? lightTheme : darkTheme;
 
   useEffect(() => {
-    setThemeMode(window.localStorage.getItem('theme') || 'light');
+    const theme = getThemeByLocalStorage();
+    setThemeMode(theme);
   }, []);
 
   return (
--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,34 +1,24 @@
 import { useCallback, useEffect, useContext } from 'react';
-import { Theme, ThemeContext } from 'src/context/ThemeProvider';
-
-enum ThemeMode {
-  LIGHT = 'light',
-  DARK = 'dark',
-}
-const LS_KEY = 'theme';
-
-const getThemeFromLocalStorage = (): ThemeMode => {
-  const theme = window.localStorage.getItem(LS_KEY);
-  return theme === ThemeMode.DARK ? ThemeMode.DARK : ThemeMode.LIGHT;
-};
+import {
+  DARK,
+  LIGHT,
+  LS_KEY,
+  Theme,
+  ThemeContext,
+} from 'src/context/ThemeProvider';
 
 const useDarkMode = (): [string | undefined, () => void] => {
   const { themeMode, setThemeMode } = useContext(ThemeContext) as Theme;
 
   const switchTheme = useCallback(() => {
-    if (themeMode === ThemeMode.LIGHT) {
-      window.localStorage.setItem(LS_KEY, ThemeMode.DARK);
-      setThemeMode(ThemeMode.DARK);
+    if (themeMode === LIGHT) {
+      localStorage.setItem(LS_KEY, DARK);
+      setThemeMode(DARK);
     } else {
-      window.localStorage.setItem(LS_KEY, ThemeMode.LIGHT);
-      setThemeMode(ThemeMode.LIGHT);
+      localStorage.setItem(LS_KEY, LIGHT);
+      setThemeMode(LIGHT);
     }
   }, [themeMode, setThemeMode]);
-
-  useEffect(() => {
-    const localTheme = getThemeFromLocalStorage();
-    setThemeMode(localTheme);
-  }, [setThemeMode]);
 
   return [themeMode, switchTheme];
 };
