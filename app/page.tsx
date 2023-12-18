292a3007860e54bb9a2d2af3b91d9691a0700ed7
Yongveloper <aaa9015@naver.com>
2023-12-18 21:35:42 +0900

    style: 불필요한 코드 삭제

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import Head from 'next/head';
 import { useState } from 'react';
 import { MainView, QuestionView } from 'components/Home';
 
@@ -10,21 +9,6 @@ export default function Home() {
   const startHandler = () => setStart(true);
 
   return (
-    <>
-      <Head>
-        <title>롤에서 알아보는 MBTI 유형 - 나에게 맞는 포지션은?</title>
-        <meta property="og:title" content="롤에서 알아보는 MBTI 유형" />
-        <meta property="og:site_name" content="롤에서 알아보는 MBTI 유형" />
-        <meta
-          property="og:image"
-          content="https://lol-mbti.vercel.app/images/logo.png"
-        />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="627" />
-        <meta property="og:image:alt" content="롤에서 알아보는 MBTI" />
-        <link rel="canonical" href="https://lol-mbti.vercel.app" />
-      </Head>
-      {start ? <QuestionView /> : <MainView startHandler={startHandler} />}
-    </>
+    <>{start ? <QuestionView /> : <MainView startHandler={startHandler} />}</>
   );
 }
