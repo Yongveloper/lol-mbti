b2e77e0e3f5e914f43b2db3b5291e4dec4245886
Yongveloper <aaa9015@naver.com>
2024-01-31 21:45:14 +0900

    feat: layout VE 적용

--- app/ClientComponentContainer.tsx
+++ app/ClientComponentContainer.tsx
@@ -1,22 +1,10 @@
 'use client';
-import styled from 'styled-components';
 import ThemeProvider from 'context/ThemeProvider';
 import GlobalStyle from 'styles/GlobalStyle';
 import Footer from 'components/common/Footer';
 import DarkModeBtn from 'components/Buttons/DarkModeBtn';
 import StyledComponentsRegistry from 'lib/registry';
-import { size } from 'styles/theme';
-const Wrapper = styled.div`
-  width: 100%;
-  max-width: ${size.mobile};
-  min-height: 100vh;
-  margin: auto;
-  padding: ${({ theme }) => theme.padding.base};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`;
-
+import * as style from './layout.css';
 interface IClientComponentContainerProps {
   children: React.ReactNode;
 }
@@ -27,12 +15,12 @@ function ClientComponentContainer({
   return (
     <StyledComponentsRegistry>
       <ThemeProvider>
-        <Wrapper>
+        <div className={style.container}>
           <GlobalStyle />
           <DarkModeBtn />
           {children}
           <Footer />
-        </Wrapper>
+        </div>
       </ThemeProvider>
     </StyledComponentsRegistry>
   );
