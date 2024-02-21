c5932d62a210c917a7376a3c41a5a7dcdeaf8e56
Yongveloper <aaa9015@naver.com>
2024-02-21 19:08:56 +0900

    feat: QuestionView 컴포넌트 VE 적용

--- src/app/_components/QuestionView/index.tsx
+++ src/app/_components/QuestionView/index.tsx
@@ -1,34 +1,12 @@
-import Button from 'src/app/_components/common/Button';
 import Content from 'src/app/_components/common/Content';
 import { questions } from 'src/data/questions';
-import styled, { css } from 'styled-components';
 import PrevNextBtn from './PrevNextBtn';
 import Progressbar from './Progressbar';
 import LoadingModal from 'src/app/_components/QuestionView/LoadingModal';
 import useQuestionnaire from 'src/hooks/useQuestionnaire';
-
-const QuestionText = styled.p`
-  width: 100%;
-  font-size: ${({ theme }) => theme.font.size.regular};
-  line-height: 1.5;
-`;
-
-const SButton = styled(Button)`
-  ${({ theme }) => {
-    return css`
-      :hover {
-        border-color: ${theme.color.bluePuple};
-        transition: border-color 0.1s linear;
-      }
-    `;
-  }}
-`;
-
-const ResultButton = styled(Button)<{ completed: boolean }>`
-  opacity: ${({ completed }) => (completed ? 1 : 0.5)};
-  cursor: ${({ completed }) => !completed && 'not-allowed'};
-  transition: opacity 0.1s ease-in-out;
-`;
+import * as style from './questionView.css';
+import { assignInlineVars } from '@vanilla-extract/dynamic';
+import { global } from 'src/styles/globalTheme.css';
 
 const QuestionView = () => {
   const {
@@ -55,36 +33,39 @@ const QuestionView = () => {
       <Progressbar current={currentQuestion} />
       <Content>
         {questions[currentQuestion].question.split('\n').map((text) => (
-          <QuestionText key={text}>{text}</QuestionText>
+          <p className={style.questionText} key={text}>
+            {text}
+          </p>
         ))}
       </Content>
       {questions[currentQuestion].answers.map((answer, index) => (
-        <SButton
+        <button
+          className={style.questionButton}
+          style={assignInlineVars({
+            [style.questionButtonFontColor]:
+              answer.type === selected[currentQuestion]
+                ? global.color.contentBackground
+                : global.color.lightBlue,
+            [style.questionButtonBackgroundColor]:
+              answer.type === selected[currentQuestion]
+                ? global.color.bluePurple
+                : global.color.contentBackground,
+          })}
           key={index}
           name={answer.type}
           onClick={onAnswerClick}
-          fontColor={
-            answer.type === selected[currentQuestion] ? 'white' : 'lightBlue'
-          }
-          bgColor={
-            answer.type === selected[currentQuestion]
-              ? 'bluePurple'
-              : 'contentBackground'
-          }
         >
           {answer.answer}
-        </SButton>
+        </button>
       ))}
       {currentQuestion === questions.length - 1 && (
-        <ResultButton
-          bgColor="lightBlue"
-          fontColor="white"
-          borderColor="lightBlue"
-          completed={completed === questions.length}
+        <button
+          className={style.resultButton}
+          disabled={completed !== questions.length}
           onClick={onResultClick}
         >
           결과 보기!
-        </ResultButton>
+        </button>
       )}
       {loading && <LoadingModal />}
     </>
