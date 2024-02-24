46cc890f5134de7ffc062b2cdc3d527387fccbdd
Yongveloper <aaa9015@naver.com>
2024-02-24 22:03:41 +0900

    style: 결과페이지 Content 컴포넌트 배경색 ThemeColor 적용

--- src/app/mbti/[type]/type.css.ts
+++ src/app/mbti/[type]/type.css.ts
@@ -1,11 +1,11 @@
 import { style } from '@vanilla-extract/css';
-import { global } from 'src/styles/globalTheme.css';
+import { global, vars } from 'src/styles/globalTheme.css';
 
 export const content = style({
   width: '100%',
   padding: `${global.padding.small} ${global.padding.base}`,
   marginBottom: global.margin.xLarge,
-  backgroundColor: global.color.contentBackground,
+  backgroundColor: vars.themeColor.color.contentBackground,
   boxShadow: global.shadow.base,
   border: `2px solid ${global.color.borderColor}`,
   textAlign: 'center',
