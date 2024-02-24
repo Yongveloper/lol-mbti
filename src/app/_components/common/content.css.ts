89c71703f7f76928d8d5d873b42ebff17961f873
Yongveloper <aaa9015@naver.com>
2024-02-24 22:02:02 +0900

    style: Content 컴포넌트 배경색 ThemeColor 적용

--- src/app/_components/common/content.css.ts
+++ src/app/_components/common/content.css.ts
@@ -1,12 +1,14 @@
 import { style } from '@vanilla-extract/css';
-import { global } from 'src/styles/globalTheme.css';
+import { global, vars } from 'src/styles/globalTheme.css';
 
 export const container = style({
   width: '100%',
   padding: `${global.padding.xLarge} ${global.padding.base}`,
   marginBottom: global.margin.xLarge,
-  backgroundColor: global.color.contentBackground,
+  backgroundColor: vars.themeColor.color.contentBackground,
   boxShadow: global.shadow.base,
-  border: `2px solid ${global.color.borderColor}`,
+  border: `2px solid ${vars.themeColor.color.borderColor}`,
+  color: vars.themeColor.color.mainFontColor,
   textAlign: 'center',
+  transition: 'all 0.25s linear',
 });
